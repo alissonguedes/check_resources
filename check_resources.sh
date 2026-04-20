e1c99f3ff629c1503b90a3b5e50490bdc097e539
Alisson Guedes <alissonguedes87@gmail.com>
2026-04-19 22:30:03 -0300

    script para coleta de recursos

--- check_resources.sh
+++ check_resources.sh
@@ -3,7 +3,7 @@
 # CPU
 read model mhz socket cores tpc threads <<< $(lscpu | awk -F: '
 /Model name/ {gsub(/^[ \t]+/, "", $2); model=$2}
-/CPU MHz/ {gsub(/^[ \t]+/, "", $2); mhz=$2}
+/CPU Mhz/ {mhz=$2}
 /Socket\(s\)/ {gsub(/^[ \t]+/, "", $2); socket=$2}
 /Core\(s\) per socket/ {gsub(/^[ \t]+/, "", $2); cores=$2}
 /Thread\(s\) per core/ {gsub(/^[ \t]+/, "", $2); tpc=$2}
@@ -11,27 +11,27 @@ read model mhz socket cores tpc threads <<< $(lscpu | awk -F: '
 END {print model, mhz, socket, cores, tpc, threads}')
 
 # Converter MHz → GHz (fallback caso não exista)
-if [ -n "$mhz" ]; then
-  freq=$(awk "BEGIN {printf \"%.2f\", $mhz/1000}")
-else
-  freq="N/A"
-fi
+#if [ -n "$mhz" ]; then
+#  freq=$(awk "BEGIN {printf \"%.2f\", $mhz/1000}")
+#else
+#  freq="N/A"
+#fi
 
 # Memória (GB)
-read ram swap <<< $(free -g | awk '
+read ram swap <<< $(free -h | awk '
 /Mem:/ {ram=$2}
 /Swap:/ {swap=$2}
 END {print ram, swap}')
 
 # Disco raiz
 read rt ru rf rp <<< $(df -h | awk '$NF=="/" {
-  gsub(/%/,"",$5);
+#  gsub(/%/,"",$5);
   print $2, $3, $4, $5
 }')
 
 # Disco backup
 backup=$(df -h | awk '$NF=="/backup" {
-  gsub(/%/,"",$5);
+#  gsub(/%/,"",$5);
   print $2";"$3";"$4";"$5
 }')
 
